906b65a3c036e02ff7b33a16c477d1148d8a4e37
caio felix <cvff12345@gmail.com>
2025-02-10 15:08:17 -0300

    fix: code review

--- domain/src/main/java/com/picpay/domain/user/User.kt
+++ domain/src/main/java/com/picpay/domain/user/User.kt
@@ -1,12 +1,16 @@
 package com.picpay.domain.user
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
+
+@Parcelize
 @Entity(tableName = "user_table")
 data class User(
     @PrimaryKey val id: Int,
     val img: String,
     val name: String,
     val username: String,
-)
+) : Parcelable
--- domain/src/main/java/com/picpay/domain/user/UserScreen.kt
+++ domain/src/main/java/com/picpay/domain/user/UserScreen.kt
@@ -1,10 +1,11 @@
 package com.picpay.domain.user
 
+import android.os.Parcelable
 import com.picpay.domain.screen.ScreenStatus
-import java.io.Serializable
+import kotlinx.parcelize.Parcelize
 
-
+@Parcelize
 data class UserScreen(
     val userList: List<User>? = null,
     val status: ScreenStatus = ScreenStatus.Loading,
-) : Serializable
+) : Parcelable
